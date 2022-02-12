9b944be971a4f41ef5ea07a516c685c9ea339390
bhagavandas <98629370+bhagavandas@users.noreply.github.com>
2022-02-12 10:02:30 +0530

    dd

---
+++ myrepo/Demo/src/com/test/DemoCricket3.java
@@ -0,0 +1,42 @@
+package com.test;
+
+public class DemoCricket3 {
+
+	public static void main(String[] args) {
+		 System.out.println("HelloWorld");
+		 
+		 DemoCricket3 obj = new DemoCricket3();
+			obj.login();
+			obj.adminTab();
+			obj.addUser();
+			obj.logout();
+			
+	
+}
+
+	
+
+	public void login() {
+		System.out.println("this is constructor");
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void addUser() {
+		System.out.println("add user");
+	}
+		// TODO Auto-generated method stub
+		
+	
+
+	public void adminTab() {
+		System.out.println("go to admintab");
+		// TODO Auto-generated method stub
+		
+	}
+	public void logout() {
+		System.out.println("logout");
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ myrepo/Demo/src/com/test/test1.java
@@ -0,0 +1,29 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+public class test1 {
+
+public void addUser() {
+	/// code
+}
+		 
+	@Test
+	public void login() {
+		System.out.println("Hello");
+		
+		DemoCricket3 obj = new DemoCricket3();
+		obj.login();
+			}
+		
+	public void login2() {
+		System.out.println("Selenium");
+	}
+		
+		
+	
+	public static void main(String[] args) {
+		 System.out.println("HelloWorld");
+	}
+}
+
