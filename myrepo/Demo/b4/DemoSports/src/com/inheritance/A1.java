07f351b181ef3aaea905c196b9b804956b482fc3
bhagavandas <98629370+bhagavandas@users.noreply.github.com>
2022-06-04 12:05:33 +0530

    Added

---
+++ myrepo/Demo/b4/DemoSports/src/com/inheritance/A1.java
@@ -0,0 +1,18 @@
+package com.inheritance;
+
+public interface A1 {
+
+
+	public void sendKeys();
+	
+	public void click();
+	
+	public void takescreenshot();
+	
+	public void get();
+	
+	public void windowHandles();
+	
+	
+	
+}
